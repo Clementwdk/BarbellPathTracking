28fb560f4617bcfa498bcc92b6f257111fd879ff
Clementwdk <wodkowskiclement@orange.fr>
2021-09-18 00:36:04 +0200

    FPS with imutils

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ import imutils
 import numpy as np
 
 video = cv2.VideoCapture('Snatch_Trim.mp4')
+
+# video.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+# video.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+print(str(video.get(3)))
+
 success, img = video.read()
-# img = cv2.resize(img1, (960, 540))
+# img = imutils.resize(img, width=640, height=480)
 boundBox = cv2.selectROI("Tracking, ", img, fromCenter=False, showCrosshair=True)
 tracker = cv2.legacy.TrackerMOSSE_create()
 tracker.init(img, boundBox)
@@ -17,24 +22,28 @@ if ( video.isOpened()== False):
 
 def trackBox(img, boundBox):
     x, y, weidth, height = int(boundBox[0]), int(boundBox[1]), int(boundBox[2]), int(boundBox[3])
-    cv2.rectangle(img,(x,y), ((x+weidth),(y+height)),(212,175,55),3,1)
+    cv2.rectangle(img,(x,y), ((x+weidth),(y+height)),(212,175,55),3,2)
     cv2.putText(img, "Barbell detected !! ", (75, 80), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
 
 
 while True:
+    fpss = FPS().start()
     timer = cv2.getTickCount()
     success, img = video.read()
 
+    fpss.update()
+    fpss.stop()
+
     success, boundBox = tracker.update(img)
     print(boundBox)
-
+    message = "{:.2f}".format(fpss.fps())
     if success == True:
         trackBox(img, boundBox)
     else:
         cv2.putText(img, "Barbell Undetected !! ", (75, 80), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
 
     fps = cv2.getTickFrequency()/(cv2.getTickCount()-timer)
-    cv2.putText(img, "FPS : " + str(fps), (75, 60), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 255), 2)
+    cv2.putText(img, "FPS : "+str(message), (75, 60), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 255), 2)
     cv2.imshow('Tracking', img)
 
 
