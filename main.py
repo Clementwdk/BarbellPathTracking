d7bd69a8a0657ccdf30b05b235a1870fe565a021
Clementwdk <wodkowskiclement@orange.fr>
2021-09-18 02:30:32 +0200

    Adding circle Barbell path

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ import cv2
 import imutils
 import numpy as np
 
-video = cv2.VideoCapture('Snatch_Trim.mp4')
+video = cv2.VideoCapture('Snatch.mp4')
 
 # video.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 # video.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 print(str(video.get(3)))
+
 
 success, img = video.read()
 # img = imutils.resize(img, width=640, height=480)
 boundBox = cv2.selectROI("Tracking, ", img, fromCenter=False, showCrosshair=True)
 tracker = cv2.legacy.TrackerMOSSE_create()
 tracker.init(img, boundBox)
+barbellPath = []
 
 if ( video.isOpened()== False):
     print("Error with the video file")
@@ -24,7 +26,13 @@ def trackBox(img, boundBox):
     x, y, weidth, height = int(boundBox[0]), int(boundBox[1]), int(boundBox[2]), int(boundBox[3])
     cv2.rectangle(img,(x,y), ((x+weidth),(y+height)),(212,175,55),3,2)
     cv2.putText(img, "Barbell detected !! ", (75, 80), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
-
+    #print(str(x )+"-----"+ str(y))
+def drawPath(img, barbellPath):
+    # coordinate = int(barbellPath[0]), int(barbellPath[1])
+    for  x in barbellPath:
+        print(x)
+        cv2.circle(img, x, 7, (255,0,0), -1)
+        #cv2.circle(img, [509,555], 7, (255,0,0),-1)
 
 while True:
     fpss = FPS().start()
@@ -35,17 +43,23 @@ while True:
     fpss.stop()
 
     success, boundBox = tracker.update(img)
-    print(boundBox)
+    barbellPath.append([int(boundBox[0]),int(boundBox[1])])
+
     message = "{:.2f}".format(fpss.fps())
     if success == True:
         trackBox(img, boundBox)
+
+
+        #cv2.circle(img, ([boundBox[0], boundBox[1]]), 7, (255, 0, 0), -1)
+
     else:
         cv2.putText(img, "Barbell Undetected !! ", (75, 80), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
 
     fps = cv2.getTickFrequency()/(cv2.getTickCount()-timer)
     cv2.putText(img, "FPS : "+str(message), (75, 60), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 255), 2)
-    cv2.imshow('Tracking', img)
+    drawPath(img, barbellPath)
 
+    cv2.imshow('Tracking', img)
 
     if (cv2.waitKey(25) & 0xFF == ord('q')):
         break
@@ -53,3 +67,4 @@ while True:
 
 video.release()
 cv2.destroyAllWindows()
+print(barbellPath)
