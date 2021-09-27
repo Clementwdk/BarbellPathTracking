62b1b5885f0651ef811cb4f075985a50ebe1dd43
Clementwdk <wodkowskiclement@orange.fr>
2021-09-27 11:35:37 +0200

    comment + clean project before submit

--- PoseEstimation.py
+++ PoseEstimation.py
@@ -1,21 +1,27 @@
 import cv2
 import mediapipe as mp
 import os
-
+import numpy as np
 
 path = 'ScreenShot'
 picfiles = [f for f in os.listdir(path) if f.endswith('.PNG')]
+filmfile = "videos/3.mp4"
 
-def bodyEstimation(pathPics):
-    mpDraw = mp.solutions.drawing_utils
-    mpPose = mp.solutions.pose
+
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+mpDrStyle = mp.solutions.drawing_styles
+
+
+def bodyEstimationPics(pathPics):
+
     pose = mpPose.Pose(static_image_mode=True,
                        model_complexity=1,
                        smooth_landmarks=True,
                        enable_segmentation=False,
                        smooth_segmentation=True,
-                       min_detection_confidence=0.7,
-                       min_tracking_confidence=0.7)
+                       min_detection_confidence=0.5,
+                       min_tracking_confidence=0.5)
     img = cv2.imread(pathPics)
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = pose.process(imgRGB)
@@ -32,9 +38,57 @@ def bodyEstimation(pathPics):
     cv2.imshow("Image", img)
     cv2.waitKey(0)
 
-print((picfiles))
-if not picfiles:
-    print("No Screen shot taken")
-else:
-    for x in picfiles:
-        bodyEstimation(path + '/' + x)
+def bodypose3d():
+    BG_COLOR = (192, 192, 192) # gray
+    IMAGE_FILES = []
+    BG_COLOR = (192, 192, 192)  # gray
+    with mpPose.Pose(
+            static_image_mode=True,
+            model_complexity=2,
+            enable_segmentation=True,
+            min_detection_confidence=0.5) as pose:
+      for idx, file in enumerate(IMAGE_FILES):
+        image = cv2.imread("ScreenShot/pic_43612.png")
+        image_height, image_width, _ = image.shape
+        # Convert the BGR image to RGB before processing.
+        results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+        if not results.pose_landmarks:
+          continue
+        print(
+            f'Nose coordinates: ('
+            f'{results.pose_landmarks.landmark[mpPose.PoseLandmark.NOSE].x * image_width}, '
+            f'{results.pose_landmarks.landmark[mpPose.PoseLandmark.NOSE].y * image_height})'
+        )
+
+        annotated_image = image.copy()
+        # Draw segmentation on the image.
+        # To improve segmentation around boundaries, consider applying a joint
+        # bilateral filter to "results.segmentation_mask" with "image".
+        condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
+        bg_image = np.zeros(image.shape, dtype=np.uint8)
+        bg_image[:] = BG_COLOR
+        annotated_image = np.where(condition, annotated_image, bg_image)
+        # Draw pose landmarks on the image.
+        mpDraw.draw_landmarks(
+            annotated_image,
+            results.pose_landmarks,
+            mpPose.POSE_CONNECTIONS,
+            landmark_drawing_spec=mpDrStyle.get_default_pose_landmarks_style())
+        cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
+        # Plot pose world landmarks.
+        mpDraw.plot_landmarks(
+            results.pose_world_landmarks, mpPose.POSE_CONNECTIONS)
+
+
+
+#bodyEstimationVideo(filmfile)
+
+# print((picfiles))
+# if not picfiles:
+#     print("No Screen shot taken")
+# else:
+#     for x in picfiles:
+#         bodyEstimationPics(path + '/' + x)
+
+bodyEstimationPics("ScreenShot/pic_443612.png")
--- main.py
+++ main.py
@@ -7,9 +7,7 @@ import pyautogui
 import mediapipe as mp
 import os
 
-video = cv2.VideoCapture('videos/7.mp4')
-
-
+video = cv2.VideoCapture('videos/3.mp4')
 success, img = video.read()
 boundBox = cv2.selectROI("Tracking, ", img, fromCenter=False, showCrosshair=True)
 tracker = cv2.legacy.TrackerMOSSE_create()
@@ -20,17 +18,17 @@ xhigh = []
 t_start =time.time()
 sec = []
 speed = []
-# img = cv2.resize(img, (640, 480))
 
-
+#Track the bounding box return text on video if detected
 def trackBox(img, boundBox):
     x, y, weidth, height = int(boundBox[0]), int(boundBox[1]), int(boundBox[2]), int(boundBox[3])
     cv2.rectangle(img,(x,y), ((x+weidth),(y+height)),(212,175,55),3,2)
     cv2.putText(img, "Barbell detected !! ", (75, 120), cv2.FONT_HERSHEY_COMPLEX, 0.7, (12,242,17), 2)
+#Draw dot on the barbell path
 def drawPath(img, barbellPath):
     for x in barbellPath:
-        cv2.circle(img, x, 3, (255,0,0), -1)
-
+        cv2.circle(img, x, 3, (0,0,255), -1)
+#Calculate the speed of the barbell
 def getVelocity(yhigh, heightBb, timeElapsed):
     yy=[]
     ymax = int(max(yhigh))
@@ -43,13 +41,33 @@ def getVelocity(yhigh, heightBb, timeElapsed):
     except:
         distcm = 0
     return distcm/timeElapsed
-
+#take screenshot during the video to analyse the body position
 def takeScreenshot():
     x= random.randrange(99999)
     image = pyautogui.screenshot()
     image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
     cv2.imwrite("ScreenShot/pic_"+str(x)+".png", image)
 
+#Create a graphic withe the move of the barbell
+def graphMove(x,y):
+    # #plotting
+    plt.plot(2,1,1)
+    plt.title("Athletes movement")
+    plt.xlabel("Xsss")
+    plt.ylabel("Ycsssss")
+    plt.xticks(rotation=180)
+    plt.plot(x, y, color ="red")
+    plt.gca().invert_yaxis()
+    plt.show()
+#Create a graphicof the speed during the lifting
+def graphSpeed(sec,speed):
+    plt.title("Speed / sec")
+    plt.xlabel("sec")
+    plt.ylabel("M/S")
+    plt.plot(sec, speed, 'blue')
+    plt.show()
+
+#Draw the pose landmarks on a pic
 def bodyPose(pathPics):
     mpDraw = mp.solutions.drawing_utils
     mpPose = mp.solutions.pose
@@ -58,8 +76,8 @@ def bodyPose(pathPics):
                        smooth_landmarks=True,
                        enable_segmentation=False,
                        smooth_segmentation=True,
-                       min_detection_confidence=0.7,
-                       min_tracking_confidence=0.7)
+                       min_detection_confidence=0.5,
+                       min_tracking_confidence=0.5)
     img = cv2.imread(pathPics)
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = pose.process(imgRGB)
@@ -76,6 +94,7 @@ def bodyPose(pathPics):
     cv2.imshow("Image", img)
     cv2.waitKey(0)
 
+#Draw the body pos in all picture inside /Screenshot
 def printBodypos():
     path = 'ScreenShot'
     picfiles = [f for f in os.listdir(path) if f.endswith('.png')]
@@ -85,6 +104,7 @@ def printBodypos():
         for x in picfiles:
             bodyPose(path + '/' + x)
 
+#Delete the images inside /ScreenShot
 def deleteScreenShot():
     dir_name = "ScreenShot"
     test = os.listdir(dir_name)
@@ -93,6 +113,7 @@ def deleteScreenShot():
         if item.endswith(".png"):
             os.remove(os.path.join(dir_name, item))
 
+#Check if the file exist
 if ( video.isOpened()== False):
     print("Error with the video file")
 
@@ -114,31 +135,37 @@ while True:
 
 
     message = cv2.getTickFrequency()/(cv2.getTickCount()-timer)
+    #Check the bounding box works
     if success == True:
         trackBox(img, boundBox)
     else:
         cv2.putText(img, "Barbell Undetected !! ", (75, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
+    #Print informations on the video time, fps and Speed
     cv2.putText(img, "Time : {0:.2f} ".format(timeElapsed), (75, 80), cv2.FONT_HERSHEY_TRIPLEX, 0.7, (76,76,76), 2)
     cv2.putText(img, "FPS : {0:.2f} s".format(message), (75, 60), cv2.FONT_HERSHEY_TRIPLEX, 0.7, (76,76,76), 2)
     cv2.putText(img, "Speed : {0:.2f} m/s".format(velocity), (75, 100), cv2.FONT_HERSHEY_TRIPLEX, 0.7, (76,76,76), 2)
 
+    #we start to collect the data when the athlete start to lift
     if velocity>0.5:
         sec.append(timeElapsed)
         speed.append(velocity)
 
     drawPath(img, barbellPath)
 
+    #Press "s" to take a screenshot
     if cv2.waitKey(25) & key == ord('s'):
         cv2.waitKey(-1)
         takeScreenshot()
         print("ScreeShot taken")
-
+    #Press "p" to pause
     if key == ord('p'):
         cv2.waitKey(-1)
-
+    #Press "q" to quit
     if cv2.waitKey(25) & key == ord('q'):
         break
+
+   #except if video is finish
     try:
         cv2.imshow('Tracking', img)
     except:
@@ -147,23 +174,8 @@ while True:
 video.release()
 cv2.destroyAllWindows()
 
-# #plotting
-plt.plot(2,1,1)
-plt.title("Athletes movement")
-plt.xlabel("Xsss")
-plt.ylabel("Ycsssss")
-plt.xticks(rotation=180)
-plt.plot(xhigh, yhigh, color ="red")
-plt.gca().invert_yaxis()
-plt.show()
-
-#plot speed
-plt.subplot(2, 1, 2)
-plt.title("Speed / sec")
-plt.xlabel("sec")
-plt.ylabel("M/S")
-plt.plot(sec, speed, 'blue')
-plt.show()
+graphSpeed(sec,speed)
+graphMove(xhigh,yhigh)
 
 printBodypos()
 deleteScreenShot()
